380c455ef0b47335a2e9eb096ec1053211443a8b
pradeepkumar673 <pradeepcastleofficial@gmail.comexit>
2025-08-21 17:07:20 +0530

    5nd commit

--- moviewebsite/src/context/WatchlistContext.jsx
+++ moviewebsite/src/context/WatchlistContext.jsx
@@ -1,1 +1,21 @@
+import React from 'react'
+import { createContext } from 'react';
 const WatchlistContext = React.createContext();
+
+const toogleWatchlist = (film) => {
+    const index = watchlist.indexOf(film);
+    if (index > -1) {
+        watchlist.splice(index, 1);
+    }
+    else {
+        watchlist.push(film);
+    }
+}
+export const WatchlistProvider = () => {
+    const [watchlist, setWatchlist] = React.useState([]);
+    return (
+        <WatchlistContext.Provider value={[watchlist, setWatchlist]}>
+            {props.children}
+        </WatchlistContext.Provider>
+    )
+}
